5a0fd82c2cd5b99c214903c0192b6b40edb27bbd
sanji118 <sanjishifa32@gmail.com>
2024-09-21 14:55:04 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,16 @@
-//search: form sumbit reloading the page
-//step-1 set event handler
-//step-2 prevent default behavior (prevent reloading browser)
+// search: form submit reloading the page
+// step - 1 set event handler
+
 document.getElementById('button-login').addEventListener('click', function(event){
-    event.preventDefault(); //vejal to beginners
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-});
+    event.preventDefault();   // step-2: prevent default behavior (prevent reloading browser)
+    const phoneNumber = document.getElementById('phone-number').value;        
+    const pinNumber = document.getElementById('pin-number').value;  // step-3: get the phone number and the pin number
+
+    if(phoneNumber=== '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href= '/home.html';// step -5: allow user to use the website
+        }
+        else{
+        alert('Wrong phone number or Pin');
+        } //bad way to validate
+})
