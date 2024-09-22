74f4df1f1bff6e0ae1bf6eefbfe36ce99200af09
sanji118 <sanjishifa32@gmail.com>
2024-09-22 11:52:44 +0600

    added config file

--- js/home.js
+++ js/home.js
@@ -2,8 +2,21 @@
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault();
     const addMoneyInput = document.getElementById('input-money').value;
-    console.log(addMoneyInput);
+    //console.log(addMoneyInput);
 
     const pinNum = document.getElementById('pin-num').value;
-    console.log(pinNum)
+    //console.log(pinNum);
+
+    if(pinNum === '1234'){
+        console.log('You have added money');
+        const balance = document.getElementById('account-balance').innerText;// get the current balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance); 
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance);
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('You have inputed wrong pin');
+    }
 })
---
+++ js/utilities.js
@@ -0,0 +1,4 @@
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
